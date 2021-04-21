63b9081e74a90bfdcc1f0cdc284d9f7064c3b0a3
laurenmenendez <menendez98@gmail.com>
2021-04-21 16:09:15 -0400

    Update create function on ui.js to display pet

--- assets/scripts/pet/ui.js
+++ assets/scripts/pet/ui.js
@@ -2,8 +2,18 @@
 
 const store = require('.././store')
 
-const onCreateSuccess = function (reponse) {
-  $('#messages').text('Pet created!')
+const onCreateSuccess = function (response) {
+  // save returned pet object on store.user
+  store.user.pet = response.pet
+  // create variable for html
+  const pet = store.user.pet
+  // add pet info to html
+  $('#messages').html(
+    `<p>Pet created!</p>
+    <p>Pet name: ${pet.name}</p>
+    <p>Pet type: ${pet.type}</p>
+    <p>Pet age: ${pet.age}</p>`
+  )
 }
 
 module.exports = {
