c70f76743611e3bf6e31c5416d0069fd7766ce1f
laurenmenendez <menendez98@gmail.com>
2021-04-21 15:28:05 -0400

    Add sign out event handler in events.js

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -38,9 +38,17 @@ const onChangePassword = function (event) {
     .catch(ui.onError)
 }
 
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onError)
+}
+
 module.exports = {
   onSignUp,
   onSignIn,
   onChangePasswordButton,
-  onChangePassword
+  onChangePassword,
+  onSignOut
 }
