e6ef6c6a66fa691c4b5d5ecdf3e450140192822f
laurenmenendez <menendez98@gmail.com>
2021-04-21 13:17:02 -0400

    Set up basic index.html & auth functions in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+const authEvents = require('./auth/events')
+const petEvents = require('./pet/events')
 
 $(() => {
-  // your JS code goes here
+  // auth events
+    $('#sign-up-form').on('submit', authEvents.onSignUp)
+    $('#sign-in-form').on('submit', authEvents.onSignIn)
+    $('#change-password-button').on('click', authEvents.onChangePasswordButton)
+    $('#change-password').on('submit', authEvents.onChangePassword)
+    $('#sign-out-button').on('click', authEvents.onSignOut)
 })
